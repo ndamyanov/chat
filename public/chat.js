6de28ef6e808ca0216e46a0020317a9da1593c62
Nikolay Damyanov <nikolay.damyanov@accedia.com>
2019-09-10 15:54:40 +0300

    change port number to 5000

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,6 @@
 
 $(function(){
-	var socket = io.connect('http://localhost:3000');
+	var socket = io.connect('http://localhost:5000');
 	
 	var message = $('#message')
 	var username = $('#username')
